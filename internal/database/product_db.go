4f4188dd3f9a0d068c5b2e15318c4520a197a42b
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-02-01 22:44:20 -0300

    feat: add product database

---
+++ internal/database/product_db.go
@@ -0,0 +1,70 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/gaspartv/API-GO-fullcycle-imersao17/internal/entity"
+)
+
+type ProductDB struct {
+	db *sql.DB
+}
+
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
+	row := pd.db.QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id =?", id)
+	var product entity.Product
+	err := row.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (pd *ProductDB) GetProductByCategory(categoryID string) ([]*entity.Product, error) {
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products WHERE category_id = ?", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []*entity.Product
+	for rows.Next() {
+		var product entity.Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+	return products, nil
+}
+
+func (pd *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
+	_, err := pd.db.Query("INSERT INTO products (id, name, description, price, category_id, image_url) VALUES ($1, $2, $3, $4, $5, $6)", product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
