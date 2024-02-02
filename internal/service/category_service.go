6bede8caac477c3d5933b4db4d8c22d8c9532d4c
Diego Gaspar <cadastro@diegogaspar.dev.br>
2024-02-01 22:53:38 -0300

    feat: add category service

---
+++ internal/service/category_service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/gaspartv/API-GO-fullcycle-imersao17/internal/database"
+	"github.com/gaspartv/API-GO-fullcycle-imersao17/internal/entity"
+)
+
+type CategoryService struct {
+	CategoryDb database.CategoryDB
+}
+
+func NewCategoryService(categoryDb database.CategoryDB) *CategoryService {
+	return &CategoryService{CategoryDb: categoryDb}
+}
+
+func (cs *CategoryService) GetCategories() ([]*entity.Category, error) {
+	categories, err := cs.CategoryDb.GetCategories()
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (cs *CategoryService) CreateCategory(name string) (*entity.Category, error) {
+	category := entity.NewCategory(name)
+	_, err := cs.CategoryDb.CreateCategory(category)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (cs *CategoryService) GetCategory(id string) (*entity.Category, error) {
+	category, err := cs.CategoryDb.GetCategory(id)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
